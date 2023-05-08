54e1d3b3f03950a896f504875a9d2983aa123fa8
qglht <quentin.guilhot@student-cs.fr>
2023-05-08 16:35:00 +0200

    saving images generated

--- src/generate_fake.py
+++ src/generate_fake.py
@@ -1,33 +1,21 @@
-import scipy.io as sio
-import os
-import pdb
+from diffusers import StableDiffusionPipeline
 
-## create mapping from ILSVRC2012_val ILSVRC2012_id 
+batch_size = 50
 
+# read file src/LOC_synset_mapping.txt that maps ILSVRC2012_synset to WordNet synset
 
-filenames = os.listdir('data/images/ILSVRC2012_img_val/')
-mapping_val_id = {filename:None for filename in filenames}
-pdb.set_trace()
-with open('src/ILSVRC2012_validation_ground_truth.txt') as f:
-    for i, line in enumerate(f):
-        mapping_val_id[filenames[i]] = int(line.strip())
+mapping_caption_wordnet = {}
+with open('src/LOC_synset_mapping.txt') as f:
+    for line in f:
+        line = line.strip().split()
+        mapping_caption_wordnet[line[0]] = line[1]
 
-## create mapping from ILSVRC2012_id to ILSVRC2012_synset
+# get all possible prompts and save them in a list
 
-# open the file src/meta.mat thanks to scipy.io 
+prompts = mapping_caption_wordnet.values()
 
-mapping_id_caption = {}
-mat_contents = sio.loadmat('src/meta.mat')
-for i in range(mat_contents['synsets'].shape[0]):
-    mapping_id_caption[mat_contents['synsets'][i][0][0][0][0]] = mat_contents['synsets'][i][0][2][0]
+# generate fake images from these prompts
 
-## create mapping from ILSVRC2012_val to ILSVRC2012_synset
-
-mapping_val_caption = {filename:mapping_id_caption[mapping_val_id[filename]] for filename in filenames}
-pdb.set_trace()
-print(mapping_val_caption)
-
-# from diffusers import StableDiffusionPipeline
-
-# pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5") # or other model
-# pipe([prompt] * batch_size)
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4") # or other model
+for prompt in prompts:
+    pipe([prompt] * batch_size)
---
+++ src/mapping.py
@@ -0,0 +1,29 @@
+import scipy.io as sio
+import os
+import pdb
+
+## create mapping from ILSVRC2012_val ILSVRC2012_id 
+
+
+filenames = os.listdir('data/images/ILSVRC2012_img_val/')
+mapping_val_id = {filename:None for filename in filenames}
+pdb.set_trace()
+with open('src/ILSVRC2012_validation_ground_truth.txt') as f:
+    for i, line in enumerate(f):
+        mapping_val_id[filenames[i]] = int(line.strip())
+
+## create mapping from ILSVRC2012_id to ILSVRC2012_synset
+
+# open the file src/meta.mat thanks to scipy.io 
+
+mapping_id_caption = {}
+mat_contents = sio.loadmat('src/meta.mat')
+for i in range(mat_contents['synsets'].shape[0]):
+    mapping_id_caption[mat_contents['synsets'][i][0][0][0][0]] = mat_contents['synsets'][i][0][2][0]
+
+## create mapping from ILSVRC2012_val to ILSVRC2012_synset
+
+mapping_val_caption = {filename:mapping_id_caption[mapping_val_id[filename]] for filename in filenames}
+pdb.set_trace()
+print(mapping_val_caption)
+
