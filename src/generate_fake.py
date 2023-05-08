1eda003e588401b0485300d036453b2223e43032
qglht <quentin.guilhot@student-cs.fr>
2023-05-08 13:53:46 +0200

    fix for type

--- src/generate_fake.py
+++ src/generate_fake.py
@@ -19,13 +19,12 @@ with open('src/ILSVRC2012_validation_ground_truth.txt') as f:
 mapping_id_caption = {}
 mat_contents = sio.loadmat('src/meta.mat')
 for i in range(mat_contents['synsets'].shape[0]):
-    pdb.set_trace()
     mapping_id_caption[mat_contents['synsets'][i][0][0][0][0]] = mat_contents['synsets'][i][0][2][0]
 
 ## create mapping from ILSVRC2012_val to ILSVRC2012_synset
 
 mapping_val_caption = {filename:mapping_id_caption[mapping_val_id[filename]] for filename in filenames}
-
+pdb.set_trace()
 print(mapping_val_caption)
 
 # from diffusers import StableDiffusionPipeline
