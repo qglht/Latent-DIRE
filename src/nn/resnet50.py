3cfef780b2d8fd916bef95631a557a34f8e549d3
Fabio Hehli <f4biohehli@hotmail.com>
2023-05-10 21:27:07 +0200

    formatting

--- src/nn/resnet50.py
+++ src/nn/resnet50.py
@@ -4,17 +4,12 @@ from torchvision.transforms import PILToTensor
 
 
 def build_resnet50_pixel(pretrained: str = True) -> ResNet:
-    '''ResNet50 with custom classifier for testing normal DIRE'''
+    """ResNet50 with custom classifier for testing normal DIRE"""
     weights = ResNet50_Weights.DEFAULT
     model = resnet50(weights=weights)
     for param in model.parameters():
         param.requires_grad = False
-    model.fc = nn.Sequential(
-        nn.Linear(2048, 128),
-        nn.ReLU(inplace=True),
-        nn.Linear(128, 2),
-        nn.Softmax(dim=1)
-    )
+    model.fc = nn.Sequential(nn.Linear(2048, 128), nn.ReLU(inplace=True), nn.Linear(128, 2), nn.Softmax(dim=1))
     model.eval()
     return model
 
@@ -26,7 +21,7 @@ def preprocess_resnet50_pixel(img):
     return batch
 
 
-def build_resnet50_latent(device: str = 'cpu', pretrained: str = True) -> ResNet:
+def build_resnet50_latent(device: str = "cpu", pretrained: str = True) -> ResNet:
     # FIXME
     pass
 
