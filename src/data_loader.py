1e83e6e9455c8298bef785426fbc7c38ebb03f04
Fabio Hehli <f4biohehli@hotmail.com>
2023-05-10 21:10:15 +0200

    use shuffle param

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from torch.utils.data import DataSet, DataLoader, random_split
+from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import ImageFolder
 
 from nn.resnet50 import preprocess_resnet50_pixel
@@ -16,8 +16,8 @@ This assumes we have organized the data in the following way:
 def get_dataloaders(root: str, batch_size: int, shuffle: bool = True) -> List[DataLoader, DataLoader, DataLoader]:
     dataset = ImageFolder(root, transform=preprocess_resnet50_pixel)
     train_dataset, val_dataset, test_dataset = random_split(dataset, lenghts=[0.8, 0.1, 0.1])
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=shuffle)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle)
 
     return train_loader, val_loader, test_loader
