88de5a49047c50193e1eea8ce7932f86e608bcb4
Fabio Hehli <f4biohehli@hotmail.com>
2023-05-11 16:09:26 +0200

    fix bug

--- scripts/dire_generation.sh
+++ scripts/dire_generation.sh
@@ -12,4 +12,4 @@
 module load gcc/8.2.0 python_gpu/3.10.4 eth_proxy
 rsync -chavzP /cluster/scratch/$USER/imagenet.tar $TMPDIR/images.tar
 tar xf $TMPDIR/images.tar --directory=$TMPDIR --checkpoint=1000 --checkpoint-action=dot
-python src/generate_dire.py --ddim-steps 10 --batch_size 20 --read_dir imagenet --write_dir_dire "/cluster/scratch/$USER/imagenet_dire" --write_dir_latent_dire "/cluster/scratch/$USER/imagenet_latent_dire"
+python src/generate_dire.py --ddim_steps 10 --batch_size 20 --read_dir imagenet --write_dir_dire "/cluster/scratch/$USER/imagenet_dire" --write_dir_latent_dire "/cluster/scratch/$USER/imagenet_latent_dire"
