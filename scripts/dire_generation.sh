7d1022158b0580e1e99fbc93d611250beabe3554
Fabio Hehli <f4biohehli@hotmail.com>
2023-05-11 23:09:35 +0200

    fix script

--- scripts/dire_generation.sh
+++ scripts/dire_generation.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-#SBATCH --gpus=a100-pcie-40gb
-#SBATCH --gpus=1
-#SBATCH --time 10:00:00
-#SBATCH --ntasks=4
-#SBATCH --mem-per-cpu=4G
 #SBATCH --job-name=compute-dire
-#SBATCH --gres=gpumem:32g
+#SBATCH --time 12:00:00
 #SBATCH --tmp=20G
+#SBATCH --ntasks=2
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gpus=1
+#SBATCH --gres=gpumem:32g
+
+COMPRESSED_FOLDER_PATH="/cluster/scratch/$USER/imagenet.tar.gz"
 
 module load gcc/8.2.0 python_gpu/3.10.4 eth_proxy
-rsync -chavzP /cluster/scratch/$USER/imagenet.tar $TMPDIR/images.tar
-mkdir $TMPDIR/images/subfolder
-tar xf $TMPDIR/images.tar $TMPDIR/images/subfolder --checkpoint=1000 --checkpoint-action=dot
+rsync -chavzP $COMPRESSED_FOLDER_PATH $TMPDIR/images.tar.gz
+mkdir -p $TMPDIR/images
+tar xzf $TMPDIR/images.tar.gz -C $TMPDIR/images 
 python src/generate_dire.py --ddim_steps 10 --batch_size 20 --write_dir_dire "/cluster/scratch/$USER/imagenet_dire" --write_dir_latent_dire "/cluster/scratch/$USER/imagenet_latent_dire"
