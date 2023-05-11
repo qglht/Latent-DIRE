3801518bba9d8cf13f6d06992842bfad9f08a424
qglht <quentin.guilhot@student-cs.fr>
2023-05-11 15:25:39 +0200

    changed path

--- src/flatten.py
+++ src/flatten.py
@@ -18,14 +18,18 @@ subfolders = os.listdir(data_path)
 for subfolder in subfolders:
     # get the path to the subfolder
     subfolder_path = os.path.join(data_path, subfolder)
+    # get the name of the subfolder alone
+    subfolder_name = subfolder.split('/')[-1]
     # get the list of files in the subfolder
     files = os.listdir(subfolder_path)
     # loop through the files
     for file in files:
         # get the path to the file
         file_path = os.path.join(subfolder_path, file)
+        print(f"this is the file path {file_path}")
         # get the path to the flattened file
-        flattened_file_path = os.path.join(flattened_path, file)
+        flattened_file_path = os.path.join(os.path.join(flattened_path, subfolder_name), file)
+        print(f"this is the flattened file path {flattened_file_path}")
         # copy the file to the flattened folder
         os.system('cp ' + file_path + ' ' + flattened_file_path)
 
