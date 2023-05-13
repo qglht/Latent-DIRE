e5095adc690e2d94adb653846382626aedf3ed80
Fabio Hehli <f4biohehli@hotmail.com>
2023-05-13 12:04:05 +0200

    fix script

--- scripts/adm_generation.sh
+++ scripts/adm_generation.sh
@@ -8,12 +8,15 @@
 #SBATCH --gpus=1
 #SBATCH --gres=gpumem:32g
 
-SAMPLE_FLAGS="--batch_size 100 --num_samples 50000 --timestep_respacing 250"
+SAMPLE_FLAGS="--batch_size 32 --num_samples 50000 --timestep_respacing 250"
 
 module load gcc/8.2.0 python_gpu/3.10.4 eth_proxy
 pip install . src/guided-diffusion
-wget --output-document models/256x256_classifier.pt https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_classifier.pt
-wget --output-document models/256x256_diffusion.pt https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion.pt
-cd src/guided-diffusion/scripts/
+if ! test -f "models/256x256_classifier.pt"; then
+    wget --output-document models/256x256_classifier.pt https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_classifier.pt
+fi
+if ! test -f "models/256x256_diffusion.pt"; then
+    wget --output-document models/256x256_diffusion.pt https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion.pt
+fi
 MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond True --diffusion_steps 1000 --image_size 256 --learn_sigma True --noise_schedule linear --num_channels 256 --num_head_channels 64 --num_res_blocks 2 --resblock_updown True --use_fp16 True --use_scale_shift_norm True"
-python classifier_sample.py $SAMPLE_FLAGS $MODEL_FLAGS --classifier_scale 1.0 --classifier_path models/256x256_classifier.pt --model_path models/256x256_diffusion.pt
+python src/guided-diffusion/scripts/classifier_sample.py --write-dir "data/adm/" $SAMPLE_FLAGS $MODEL_FLAGS --classifier_scale 1.0 --classifier_path models/256x256_classifier.pt --model_path models/256x256_diffusion.pt
