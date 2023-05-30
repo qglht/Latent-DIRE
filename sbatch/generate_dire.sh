a462a337fa396db3a1320845d7dc562ac37f78a2
Lucien ColonnaWalewski <lcolonn@eu-login-18.euler.ethz.ch>
2023-05-30 17:24:08 +0200

    Updated dire generation scripts

--- sbatch/generate_dire.sh
+++ sbatch/generate_dire.sh
@@ -17,7 +17,7 @@ mkdir -p $TMPDIR/images
 tar xf $TMPDIR/images.tar -C $TMPDIR/images 
 # make sure you have a few GB of space in your home directory as we save ldire as pt files there 
 # you can check with lquota
-python scripts/generate_dire.py --ddim_steps 10 --batch_size 20 \
---write_dir_dire "/cluster/scratch/$USER/sd_generated_dire_10_steps_v2" \
---write_dir_ldire "/cluster/scratch/$USER/sd_generated_ldire_10_steps_v2" \
---write_dir_decoded_ldire "/cluster/scratch/$USER/sd_generated_decoded_ldire_10_steps_v2"
+python scripts/generate_dire.py --ddim_steps 20 --batch_size 10 \
+--write_dir_dire "/cluster/scratch/$USER/sd_dire_20" \
+--write_dir_ldire "/cluster/scratch/$USER/sd_ldire_20" \
+--write_dir_decoded_ldire "/cluster/scratch/$USER/sd_dldire_20"
--- sbatch/generate_dire_adm.sh
+++ sbatch/generate_dire_adm.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=compute-dire
+#SBATCH --job-name=compute-adm-dire
 #SBATCH --time 12:00:00
 #SBATCH --tmp=20G
 #SBATCH --ntasks=2
 #SBATCH --mem-per-cpu=4G
-#SBATCH --gpus=a100_80gb:1
+#SBATCH --gpus=1
+#SBATCH --gres=gpumem:32g
 
-COMPRESSED_FOLDER_PATH="/cluster/scratch/$USER/dalle_2.tar.gz"
+COMPRESSED_FOLDER_PATH="/cluster/scratch/$USER/sd_generated.tar.gz"
 
 module load gcc/8.2.0 python_gpu/3.10.4 eth_proxy
 wandb login
@@ -15,4 +16,5 @@ pip install . src/guided-diffusion
 rsync -chavzP $COMPRESSED_FOLDER_PATH $TMPDIR/images.tar
 mkdir -p $TMPDIR/images
 tar xf $TMPDIR/images.tar -C $TMPDIR/images 
-python scripts/generate_dire.py --ddim_steps 10 --batch_size 20 --write_dir_dire "/cluster/scratch/$USER/dalle_2_dire_10_steps" --write_dir_latent_dire "/cluster/scratch/$USER/dalle_2_latent_dire_10_steps"
+python scripts/generate_dire.py --model_id "models/lsun_bedroom.pt" --ddim_steps 20 --batch_size 10 \
+--write_dir_dire "/cluster/scratch/$USER/sd_admdire_20" \
