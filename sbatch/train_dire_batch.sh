84ae6037973c0b9f0403dc29252bf12bb348e67c
Lucien ColonnaWalewski <lcolonn@eu-login-03.euler.ethz.ch>
2023-06-02 16:59:01 +0200

    Fixed bug in batch trainer related to rsync directories

--- sbatch/train_dire_batch.sh
+++ sbatch/train_dire_batch.sh
@@ -5,34 +5,35 @@
 # each differing in the number of ddim steps.
 
 #SBATCH --job-name=train_batch
-#SBATCH --time 12:00:00
+#SBATCH --time 4:00:00
 #SBATCH --tmp=20G
 #SBATCH --ntasks-per-node=4
 #SBATCH --mem-per-cpu=4G
 #SBATCH --gpus=1
-#SBATCH --gres=gpumem:32g
+#SBATCH --gres=gpumem:8g
 
 module load gcc/8.2.0 python_gpu/3.10.4 eth_proxy
 pip install . src/guided-diffusion
 
 # Define the list of datasets
-datasets=("dire_5" "dire_10" "dire_20" "dire_30")
+datasets=("5_steps" "10_steps" "20_steps" "30_steps")
 
 # Loop over the datasets
 for dataset in "${datasets[@]}"; do
     echo "Training model resnet50_pixel on dataset $dataset"
 
-    COMPRESSED_FOLDER_PATH = "/cluster/scratch/$USER/training_data/$dataset.tar"
+    COMPRESSED_FOLDER_PATH="/cluster/scratch/$USER/training_data/dire/$dataset.tar"
     # Rsync the dataset to the current directory
-    rsync -chavzP $COMPRESSED_FOLDER_PATH $TMPDIR/dataset.tar
-    mkdir -p $TMPDIR/dataset
-    tar xf $TMPDIR/dataset.tar -C $TMPDIR/dataset
+    rsync -chavzP $COMPRESSED_FOLDER_PATH $TMPDIR
+    tar xf $TMPDIR/$dataset.tar -C $TMPDIR
 
     # Train the model
     NAME="DIRE 10k resnet50_pixel $dataset"
     MODEL="resnet50_pixel"
     DATA_TYPE="images" # images or latent
-    DATA="$TMPDIR/dataset"
+    DATA="$TMPDIR/$dataset"
+    ls $TMPDIR
+    ls $DATA
     python src/training.py \
     --name "$NAME" \
     --model $MODEL \
