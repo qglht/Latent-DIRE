2ab9ddbf0a1e09902e9117582b9852e51e87bed0
lucienwalewski <lcolonn@student.ethz.ch>
2023-06-04 15:00:06 +0200

    Updated dataloader to recognise directories which already contains splits.

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,4 +1,5 @@
-from typing import Callable, Tuple
+from typing import Callable, Tuple, Optional
+import os
 
 import torch
 from torch.utils.data import DataLoader, random_split
@@ -11,6 +12,12 @@ This assumes we have organized the data in the following way:
     root
     ├── real
     └── fake
+or in the following way:
+    root
+    ├── train
+    ├── val
+    └── test
+We automatically determine the type of data based on the folder structure.
 """
 
 
@@ -19,19 +26,43 @@ def ldire_loader(path: str) -> torch.Tensor:
 
 
 def get_dataloaders(
-    root: str, model: str, type: str, batch_size: int, num_workers: int = 0
+    root: str,
+    model: str,
+    type: str,
+    batch_size: int,
+    num_workers: int = 0,
+    split: Optional[Tuple[float, float, float]] = [0.8, 0.1, 0.1],
 ) -> Tuple[DataLoader, DataLoader, DataLoader]:
+
+    # Determine transform
     if model in ["resnet50_latent"]:
         transform = preprocess_resnet50_latent
     elif model in ["resnet50_pixel"]:
         transform = preprocess_resnet50_pixel
-    if type == "images":
-        dataset = ImageFolder(root, transform=transform)
-    elif type == "latent":
-        dataset = DatasetFolder(root, loader=ldire_loader, extensions=(".pt",))
-    train_dataset, val_dataset, test_dataset = random_split(dataset, lengths=[0.8, 0.1, 0.1])
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
+    
+    # Determine folder structure
+    if set(os.listdir(root)) == set(["train", "val", "test"]):
+        if type == "images":
+            train_dataset = ImageFolder(f"{root}/train", transform=transform)
+            val_dataset = ImageFolder(f"{root}/val", transform=transform)
+            test_dataset = ImageFolder(f"{root}/test", transform=transform)
+        elif type == "latent":
+            train_dataset = DatasetFolder(f"{root}/train", loader=ldire_loader, extensions=(".pt",))
+            val_dataset = DatasetFolder(f"{root}/val", loader=ldire_loader, extensions=(".pt",))
+            test_dataset = DatasetFolder(f"{root}/test", loader=ldire_loader, extensions=(".pt",))
+    else:
+        if type == "images":
+            dataset = ImageFolder(root, transform=transform)
+        elif type == "latent":
+            dataset = DatasetFolder(root, loader=ldire_loader, extensions=(".pt",))
+        train_dataset, val_dataset, test_dataset = random_split(
+            dataset, lengths=split)
+
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=True)
+    val_loader = DataLoader(
+        val_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
+    test_loader = DataLoader(
+        test_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
 
     return train_loader, val_loader, test_loader
