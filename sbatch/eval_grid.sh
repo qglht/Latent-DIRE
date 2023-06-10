6b65407afb11b3572cd1e16dfc5891efa18c0614
lucienwalewski <lcolonn@student.ethz.ch>
2023-06-10 16:13:19 +0200

    Updated gpu mem 32g -> 24g

--- sbatch/eval_grid.sh
+++ sbatch/eval_grid.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --mem-per-cpu=4G
 #SBATCH --gpus=1
-#SBATCH --gres=gpumem:32g
+#SBATCH --gres=gpumem:24g
 
 module load gcc/8.2.0 python_gpu/3.10.4 eth_proxy
 pip install . src/guided-diffusion
